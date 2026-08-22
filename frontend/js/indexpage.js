54b8303fe377fa0b50c0a3ed30f42b423c3d25a9
Lijan Baral <lijanbaral20@gmail.com>
2026-08-23 00:01:43 +1000

    fixed

--- frontend/js/indexpage.js
+++ frontend/js/indexpage.js
@@ -108,7 +108,7 @@ function createFeaturedProductCard(
                         type="button"
                         class="btn btn-primary"
                         data-product-id="${product.product_id}"
-                        onclick="handleAddToCart(${product.product_id})"
+                        onclick="handleFeaturedAddToCart(${product.product_id})"
                     >
                         Add to Cart
                     </button>
@@ -153,7 +153,6 @@ function handleFeaturedAddToCart(productId) {
 }
 
 window.handleFeaturedAddToCart = handleFeaturedAddToCart;
-
 /* =========================================================
    IMAGE URL
 ========================================================= */
